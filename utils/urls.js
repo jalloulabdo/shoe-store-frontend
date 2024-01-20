577deaa1870d65e81c9a5f84386a0a4c21896209
jalloulabdo <jalloulabdo88@gmail.com>
2024-01-20 15:15:19 +0100

    api urls

--- utils/urls.js
+++ utils/urls.js
@@ -1,3 +1,3 @@
 export const STRAPI_API_TOKEN = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
-export const API_URL = "https://shoe-store-i7wq.onrender.com/";
+export const API_URL = "https://shoe-store-i7wq.onrender.com";
